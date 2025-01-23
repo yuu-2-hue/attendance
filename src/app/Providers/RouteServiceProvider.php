caee9859886d5b75929cbae7c0803d80d8a8b39f
yamamoto2 <f.yuu1128.yuu1128@gmail.com>
2025-01-24 00:20:32 +0900

    RouteServiceProviderの修正

--- src/app/Providers/RouteServiceProvider.php
+++ src/app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/attendance';
+    public const HOME = '/attendance/list';
 
     /**
      * The controller namespace for the application.
